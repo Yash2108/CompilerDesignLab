2f8eecda24211df16a553af67109eedf61293f66
Yash2108 <1999yashjain@gmail.com>
2020-11-01 13:53:04 +0530

    Added Operator Function Table. Added functionality to check for cycles in the graph.

--- Operator Precedence/BT18GCS008 - Operator Precedence.py
+++ Operator Precedence/BT18GCS008 - Operator Precedence.py
@@ -58,7 +58,7 @@ elif operatorGrammar==0:
     exit("Not Operator Grammar")
 operators={'$':0}
 associativity={'$':'-'}
-terminals=[j for i in grammar.values() for j in i if j.islower()]
+terminals=[j for i in grammar.values() for k in i for j in k if j.islower()]
 operatorPriority={'+': 2,'-':1, '*':3, '/':4,'%':5, '^':6, '$':0, '(':7, ')':7}
 operatorAssociativity={'+': '>','-':'>', '*':'>', '/':'>','%':'>', '^':'<', '$':0,'(':'>', ')':'>'}
 for i in terminals:
@@ -90,7 +90,7 @@ for i in grammar.items():
             val=count+operatorPriority[j]
 
     count=val
-precedence={i: {j:'' for j in operators}for i in operators}
+precedence={i: {j:['-'] for j in operators}for i in operators}
 for i in precedence:
     for j in precedence[i]:
         if i==j:
@@ -100,7 +100,6 @@ for i in precedence:
         elif operators[i]<operators[j]:
             precedence[i][j]=['<']
         
-# print(precedence)
 row=[]
 for i in precedence.items():
     data_row=i[1]
@@ -108,5 +107,88 @@ for i in precedence.items():
     data_row['Operator']=operator
     row.append(data_row)
 precedence_df=pd.DataFrame(row, index=precedence.keys(),columns=operators)
+for i in precedence:
+    precedence[i].pop('Operator')
 print("Precedence Table:")
 print(precedence_df)
+
+def find_all_paths(graph, start, end, path=[]):
+    if start in path:
+        print('There is a cycle in the graph. Cycle:\n', path)
+        exit()
+    path = path + [start]
+    if start == end:
+        return [path]
+
+    if start not in graph.keys():
+        return []
+    paths = []
+    for node in graph[start]:
+        if node not in path:
+            newpaths = find_all_paths(graph, node, end, path)
+            for newpath in newpaths:
+                paths.append(newpath)
+    # print(start+' '+end, end=' ')
+    # print(paths)
+    return paths
+
+func_table={'f': {i: ''for i in precedence.keys()}, 'g': {i: ''for i in precedence.keys()}}
+graph={'f('+i+')': ''for i in precedence.keys()}
+graph={'g('+i+')': ''for i in precedence.keys()}
+for i in precedence:
+    outs=[]
+    for j in precedence[i]:
+        if precedence[i][j]==['>']:
+            if 'g('+j+')' not in outs:
+                outs.append('g('+j+')')
+    graph['f('+i+')']=outs
+    
+for i in precedence:
+    outs=[]
+    for j in precedence[i]:
+        if precedence[j][i]==['<']:
+            if 'f('+i+')' not in outs:
+                outs.append('f('+j+')')
+    graph['g('+i+')']=outs
+
+for i in precedence:
+    cost=0
+    path=[]
+    for j in precedence:
+        paths=find_all_paths(graph, 'f('+i+')', 'g('+j+')')
+        for k in paths:
+            if len(k)>cost:
+                cost=len(k)
+                path=k
+    for j in precedence:
+        paths=find_all_paths(graph, 'f('+i+')', 'f('+j+')')
+        for k in paths:
+            if len(k)>cost:
+                cost=len(k)
+                path=k
+    cost-=1
+    func_table['f'][i]=cost
+    # print(path, end='---->')
+    # print('f('+i+')--->'+str(cost))
+
+for i in precedence:
+    cost=0
+    path=[]
+    for j in precedence:
+        paths=find_all_paths(graph, 'g('+i+')', 'f('+j+')')
+        for k in paths:
+            if len(k)>cost:
+                cost=len(k)
+                path=k
+    for j in precedence:
+        paths=find_all_paths(graph, 'g('+i+')', 'g('+j+')')
+        for k in paths:
+            if len(k)>cost:
+                cost=len(k)
+                path=k
+    cost-=1
+    func_table['g'][i]=cost
+    # print(path, end='---->')
+    # print('g('+i+')--->'+str(cost))
+func_table_df=pd.DataFrame(func_table).transpose()
+print(func_table_df)
